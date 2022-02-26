a3add45a507d137a9529dbd03fa8394030e9908b
gargar77 <gbautista2@knights.ucf.edu>
2022-02-26 01:46:58 -0500

    removed unecessary console logs

--- pages/components/utils/imageDataScraper.js
+++ pages/components/utils/imageDataScraper.js
@@ -1,6 +1,5 @@
 
 const imageDataScraper = (bitmap) => {
-  
       const canvas = document.createElement('canvas');
       const context = canvas.getContext('2d');
       context.drawImage(bitmap,0,0);
