c3693298d66a1d510970903525d6015d016afe68
gargar77 <gbautista2@knights.ucf.edu>
2022-02-26 03:21:41 -0500

    video capture can interpret color with sound

--- components/videoCapture.js
+++ components/videoCapture.js
@@ -1,9 +1,9 @@
-import {useRef, useState} from 'react'
+import {useRef, useState, useEffect} from 'react'
 
 import Webcam from  'react-webcam'
 import getImageData from '../utils/imageDataScraper';
 import {calculateDominantColor, getRGBsums} from '../utils/colorAnalyzer';
-
+import frequencyMap from '../frequencyMap';
 const videoConstraints = {
     width: 200,
     height: 200,
@@ -14,14 +14,35 @@ export default function VideoCapture() {
     const webcamRef = useRef(null);
     const [analyzingColor, setAnalyzingColor] = useState(false);
     const [intervalId, setIntervalId] = useState(null);
-    const [rgbSums, setRgbSums] = useState([0,0,0]);
     const [readyToAnalyze, setReadyToAnalyze] = useState(false);
+    const [audioSource, setAudioSource] = useState(null);
+    const [dominantColor, setDominantColor] = useState("");
+
+    useEffect(() => {
+            if (!readyToAnalyze) return;
+            if (!dominantColor) return dominantColor;
+            if (!audioSource) {
+                startAudio(dominantColor)   
+            } else {
+                audioSource.frequency.value = frequencyMap[dominantColor]
+            }
+        },[dominantColor])
+
+     const startAudio = (dominant) => {
+        const audioContext = new AudioContext();
+        let source = audioContext.createOscillator();
+        source.frequency.value = frequencyMap[dominant];
+        source.connect(audioContext.destination);
+        setAudioSource(source);
+        source.start();
+    };
 
     const getVideoFrameData = async (imageCapture) => {
         let imageBitmap = await imageCapture.grabFrame();
         let imageData = getImageData(imageBitmap).data;
-        setRgbSums(getRGBsums(imageData));
-    }
+        let rgbSums = getRGBsums(imageData);
+        setDominantColor(calculateDominantColor(rgbSums));
+    };
 
     const record = async () => {
         setAnalyzingColor(true);
@@ -33,8 +54,11 @@ export default function VideoCapture() {
     };
     const stopRecord = () => {
         clearInterval(intervalId);
-        setIntervalId(false);
+        setIntervalId(null);
         setAnalyzingColor(false);
+        audioSource.stop();
+        setAudioSource(null);
+        setDominantColor("")
     }
 
     return (
@@ -45,7 +69,7 @@ export default function VideoCapture() {
             ref={webcamRef}
             onUserMedia={() => setReadyToAnalyze(true)}
             />
-        <p>{analyzingColor ? calculateDominantColor(rgbSums) : ""}</p>
+        <p>{analyzingColor ?  dominantColor : ""}</p>
         {readyToAnalyze ?
         <div> 
             <button disabled={analyzingColor} onClick={record}>Analyze</button>
