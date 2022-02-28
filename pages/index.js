59c1db80659a400706d64e63adbd1ae0fc9d2624
gargar77 <gbautista2@knights.ucf.edu>
2022-02-28 12:46:20 -0500

    styled background

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react'
 
 import Head from 'next/head'
 import {Container, Box} from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
+import { Button, Image } from '@chakra-ui/react'
+import colorfulBackground from '../public/colorful_background_web.jpg'
 
 import SynesthesiaSimulator from '../components/synesthesiaSimulator'
 
@@ -10,24 +11,27 @@ import SynesthesiaSimulator from '../components/synesthesiaSimulator'
 
 export default function Home() {
   const [isRecording,setIsRecording] = useState(false);
-
   return (
     <div>
       <Head>
         <title>Synesthesia</title>
         <meta name="description" content="synethesia simulator" />
       </Head>
+      <Box position="absolute"zIndex={-1}>
+       <Image fit="cover" heigth="100%" src={colorfulBackground.src}/>
+      </Box>
+      <main>
         {isRecording ?
-        <Container centerContent borderRadius="1g"  marginTop={12}>
-          <Box maxW={400} maxH={300}><SynesthesiaSimulator stopWebcam={() => setIsRecording(false)}/></Box>
-        </Container>
-         :
-         <Container centerContent borderRadius="1g"  marginTop={12}>
-           <Box height={300}></Box>
-           <Button onClick={()=> setIsRecording(true)}>Start</Button>
-         </Container>
-         
+          <Container centerContent>
+            <Box maxW={400} maxH={300}><SynesthesiaSimulator stopWebcam={() => setIsRecording(false)}/></Box>
+          </Container>
+          :
+          <Container centerContent>
+            <Box height={300}></Box>
+            <Button onClick={()=> setIsRecording(true)}>Start</Button>
+          </Container>
         }
+      </main>
       <footer>
      
       </footer>
