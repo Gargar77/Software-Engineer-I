90782bed3e8553d2ce9c2055dd198b7639dd7783
gargar77 <gbautista2@knights.ucf.edu>
2022-02-27 23:19:24 -0500

    added luminance conversion functions

--- utils/colorAnalyzer.js
+++ utils/colorAnalyzer.js
@@ -23,6 +23,17 @@ export const calculateDominantColor = (rgbSums) => {
     return null;
 }
 
+const  _getLuminance = (rgbArray) => {
+    return (0.299 * rgbArray[0]) + (0.587 * rgbArray[1]) + (0.114 * rgbArray[2])
+}
+
+const _convertLuminanceToFrequency = (luminance) => {
+    return ((luminance * 800) / 255).toFixed(2)
+}
+
+export const convertRgbToFrequency = (rgbArray) => {
+    return _convertLuminanceToFrequency(_getLuminance(rgbArray))
+};
 //method 2: return avg rgb value
 export const getAvgRGBAValue = (colorDataArray) => {
     const rgbaSums = [0,0,0,0];
