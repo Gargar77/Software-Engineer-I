0d70b4494baa198bb501b71a50ab95791675eb4c
gargar77 <gbautista2@knights.ucf.edu>
2022-03-04 21:18:47 -0500

    added refferences

--- utils/audioSampler.js
+++ utils/audioSampler.js
@@ -1,5 +1,5 @@
 import {convertRgbToFrequency} from './colorAnalyzer'
-
+//source: https://teropa.info/blog/2016/08/30/amplitude-and-loudness.html
 export const startAudio = (rgbaValue, setAudioSource) => {
     const audioContext = new AudioContext();
     const oscillator = new OscillatorNode(audioContext);
